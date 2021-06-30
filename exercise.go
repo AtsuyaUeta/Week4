83d675e07cadf08abc3a32ac543446f2b6500957
Atsuya Ueta <m215670@hiroshima-u.ac.jp>
2021-06-30 03:04:35 +0000

    modified files

--- exercise.go
+++ exercise.go
@@ -14,14 +14,14 @@ func (b *Board) put(x, y int, u string) {
 	if u == "o" {
 		b.tokens[x+3*y] = 1
 	} else if u == "x" {
-		b.tokens[x+3*y] = 0
+		b.tokens[x+3*y] = 10
 	}
 }
 
 func (b *Board) get(x, y int) string {
 	if b.tokens[x+3*y] == 1 {
 		return "o"
-	} else if b.tokens[x+3*y] == 0 {
+	} else if b.tokens[x+3*y] == 10 {
 		return "x"
 	} else {
 		return "."
@@ -35,6 +35,27 @@ func printBoard(b *Board) {
 		}
 		fmt.Printf("\n")
 	}
+}
+
+func judge(b *Board) int {
+	sums := [8]int{0, 0, 0, 0, 0, 0, 0, 0}
+	sums[0] = b.tokens[0] + b.tokens[1] + b.tokens[2]
+	sums[1] = b.tokens[3] + b.tokens[4] + b.tokens[5]
+	sums[2] = b.tokens[6] + b.tokens[7] + b.tokens[8]
+	sums[3] = b.tokens[0] + b.tokens[3] + b.tokens[6]
+	sums[4] = b.tokens[1] + b.tokens[4] + b.tokens[7]
+	sums[5] = b.tokens[2] + b.tokens[5] + b.tokens[8]
+	sums[6] = b.tokens[0] + b.tokens[4] + b.tokens[8]
+	sums[7] = b.tokens[2] + b.tokens[4] + b.tokens[6]
+	for _, v := range sums {
+		if v == 3 {
+			return 1
+		} else if v == 30 {
+			return 2
+		}
+	}
+	return 0
+
 }
 
 func main() {
@@ -59,5 +80,18 @@ func main() {
 			b.put(x, y, "x")
 		}
 		printBoard(b)
+
+		v := judge(b)
+		if v == 1 {
+			fmt.Printf("Player 1 won\n")
+			break
+		} else if v == 2 {
+			fmt.Printf("Player 2 won\n")
+			break
+		}
+	}
+	v := judge(b)
+	if v == 0 {
+		fmt.Printf("Draw\n")
 	}
 }
--- exercise_test.go
+++ exercise_test.go
@@ -21,3 +21,33 @@ func TestPutToken02(t *testing.T) {
 		t.Errorf("....")
 	}
 }
+
+func TestPutToken03(t *testing.T) {
+	b := &Board{
+		tokens: []int{1, 10, 1, 10, 1, 10, 10, 1, 1},
+	}
+	v := judge(b)
+	if v != 1 {
+		t.Errorf("....")
+	}
+}
+
+func TestPutToken04(t *testing.T) {
+	b := &Board{
+		tokens: []int{1, 10, 1, 1, 10, 0, 0, 10, 0},
+	}
+	v := judge(b)
+	if v != 2 {
+		t.Errorf("....")
+	}
+}
+
+func TestPutToken05(t *testing.T) {
+	b := &Board{
+		tokens: []int{1, 10, 1, 1, 10, 10, 10, 1, 1},
+	}
+	v := judge(b)
+	if v != 0 {
+		t.Errorf("....")
+	}
+}
